0bb2a67ee73c94503ce7c4b89288c551d180a13a
sudhdkso <waga1009@gmail.com>
2021-09-30 13:08:49 +0900

    git

---
+++ src/main/java/com/project/capsback/CapsBackApplication.java
@@ -0,0 +1,13 @@
+package com.project.capsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapsBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/capsback/CapsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.capsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

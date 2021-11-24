0a11bc436937ceedf38a0e331cf56819a710b9c4
sudhdkso <78727847+sudhdkso@users.noreply.github.com>
2021-11-24 13:58:10 +0900

    asdf

--- src/main/java/com/project/capsback/entity/Route.java
+++ src/main/java/com/project/capsback/entity/Route.java
@@ -17,8 +17,7 @@ public class Route implements Serializable{
     @Id
     private Date routeCoverageDate;
 
-    @Id
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "routeNumber",referencedColumnName = "routeNumber")
     private BusAssignment busAssignment;
 
